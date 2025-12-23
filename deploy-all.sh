e96ed815785e78142b4ade8dd5604613c1870719
Warren Roque <wroquem@gmail.com>
2025-12-23 02:59:41 -0500

    Add files via upload

---
+++ deploy-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo ">>> Iniciando despliegue completo..."
+
+# 1. Configuración básica
+kubectl apply -f configmap.yml
+kubectl apply -f secret.yaml
+
+# 2. Base de Datos con Persistencia
+kubectl apply -f mysql-pvc.yml
+kubectl apply -f mysql-deployment.yml
+kubectl apply -f mysql-service.yml
+
+echo ">>> Esperando a que MySQL esté Ready..."
+kubectl rollout status deployment/mysql-deployment
+
+# 3. Aplicación Python REST
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
+kubectl apply -f HorizontalPodAutoscal.yml
+kubectl apply -f ingress.yml
+
+echo ">>> Esperando a que la Aplicación esté Ready..."
+kubectl rollout status deployment/python-rest-deployment
+
+echo ">>> DESPLIEGUE FINALIZADO EXITOSAMENTE"
+kubectl get ingress
