ebbe4e28bbc0d1ea44ada0a02cbbe192c0563fa4
Warren Roque <wroquem@gmail.com>
2025-12-23 12:27:09 -0500

    Update init.sql

--- docker-mysql/init-scripts/init.sql
+++ docker-mysql/init-scripts/init.sql
@@ -4,4 +4,8 @@ CREATE TABLE IF NOT EXISTS usuarios (
     email VARCHAR(100)
 );
 
+
 INSERT INTO usuarios (nombre, email) VALUES ('Juan Perez', 'juan@example.com'), ('Maria Lopez', 'maria@example.com');
+INSERT INTO usuarios (nombre, email) VALUES ('Warren Roque', 'warren@example.com'), ('Mery Maguina', 'mery@example.com');
+INSERT INTO usuarios (nombre, email) VALUES ('Facundo Rodriguez', 'Facundo@example.com'), ('Julian Martinez', 'Julian@example.com');
+INSERT INTO usuarios (nombre, email) VALUES ('Emilio Cortez', 'Emilio@example.com'), ('Justo Lopez', 'justo@example.com');
