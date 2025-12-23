bd0059f069113b009959fba61244606507252467
Warren Roque <wroquem@gmail.com>
2025-12-23 05:34:11 -0500

    Add files via upload

--- docker-mysql/init-scripts/init.sql
+++ docker-mysql/init-scripts/init.sql
@@ -1,1 +1,7 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    email VARCHAR(100)
+);
 
+INSERT INTO usuarios (nombre, email) VALUES ('Juan Perez', 'juan@example.com'), ('Maria Lopez', 'maria@example.com');
